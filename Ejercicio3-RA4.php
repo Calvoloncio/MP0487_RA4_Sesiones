223ce36d8c92e7c9bb877f0a8598ff0cfc211a1e
Calvoloncio <xavichavochavuski@gmail.com>
2025-03-10 09:58:31 +0100

    Cambio 10/3/2025

--- Ejercicio3-RA4.php
+++ Ejercicio3-RA4.php
@@ -84,15 +84,12 @@ if (!empty($_SESSION['list'])) {
 <head>
     <title>Shopping list</title>
     <style>
-        table,
-        th,
-        td {
+        table,th,td {
             border: 1px solid black;
             border-collapse: collapse;
         }
 
-        th,
-        td {
+        th,td {
             padding: 5px;
         }
 
@@ -106,21 +103,21 @@ if (!empty($_SESSION['list'])) {
     <h1>Shopping list</h1>
     <form method="post">
         <label for="name">name:</label>
-        <input type="text" name="name" id="name" value="<?php echo htmlspecialchars($name); ?>">
+        <input type="text" name="name" id="name" value="<?php echo ($name); ?>">
         <br>
         <label for="quantity">quantity:</label>
-        <input type="number" name="quantity" id="quantity" value="<?php echo htmlspecialchars($quantity); ?>">
+        <input type="number" name="quantity" id="quantity" value="<?php echo ($quantity); ?>">
         <br>
         <label for="price">price:</label>
-        <input type="number" name="price" id="price" value="<?php echo htmlspecialchars($price); ?>">
+        <input type="number" name="price" id="price" value="<?php echo ($price); ?>">
         <br>
-        <input type="hidden" name="index" value="<?php echo htmlspecialchars($index); ?>">
+        <input type="hidden" name="index" value="<?php echo ($index); ?>">
         <input type="submit" name="add" value="Add">
         <input type="submit" name="update" value="Update">
         <input type="submit" name="reset" value="Reset">
     </form>
-    <p style="color:red;"><?php echo htmlspecialchars($error); ?></p>
-    <p style="color:green;"><?php echo htmlspecialchars($message); ?></p>
+    <p style="color:red;"><?php echo ($error); ?></p>
+    <p style="color:green;"><?php echo ($message); ?></p>
     <table>
         <thead>
             <tr>
@@ -134,16 +131,16 @@ if (!empty($_SESSION['list'])) {
         <tbody>
             <?php foreach ($_SESSION['list'] as $index => $item) { ?>
                 <tr>
-                    <td><?php echo htmlspecialchars($item['name']); ?></td>
-                    <td><?php echo htmlspecialchars($item['quantity']); ?></td>
-                    <td><?php echo htmlspecialchars($item['price']); ?></td>
-                    <td><?php echo htmlspecialchars($item['quantity'] * $item['price']); ?></td>
+                    <td><?php echo ($item['name']); ?></td>
+                    <td><?php echo ($item['quantity']); ?></td>
+                    <td><?php echo ($item['price']); ?></td>
+                    <td><?php echo ($item['quantity'] * $item['price']); ?></td>
                     <td>
                         <form method="post">
-                            <input type="hidden" name="name" value="<?php echo htmlspecialchars($item['name']); ?>">
-                            <input type="hidden" name="quantity" value="<?php echo htmlspecialchars($item['quantity']); ?>">
-                            <input type="hidden" name="price" value="<?php echo htmlspecialchars($item['price']); ?>">
-                            <input type="hidden" name="index" value="<?php echo htmlspecialchars($index); ?>">
+                            <input type="hidden" name="name" value="<?php echo ($item['name']); ?>">
+                            <input type="hidden" name="quantity" value="<?php echo ($item['quantity']); ?>">
+                            <input type="hidden" name="price" value="<?php echo ($item['price']); ?>">
+                            <input type="hidden" name="index" value="<?php echo ($index); ?>">
                             <input type="submit" name="edit" value="Edit">
                             <input type="submit" name="delete" value="Delete">
                         </form>
@@ -152,7 +149,7 @@ if (!empty($_SESSION['list'])) {
             <?php } ?>
             <tr>
                 <td colspan="3" align="right"><strong>Total:</strong></td>
-                <td><?php echo htmlspecialchars($totalValue); ?></td>
+                <td><?php echo ($totalValue); ?></td>
                 <td>
                     <form method="post">
                         <input type="submit" name="total" value="Calculate total">
